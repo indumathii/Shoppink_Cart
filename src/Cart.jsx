c110b75dc8c0d9fafce9c9b4222531d0faae952d
indumathii <neelaindumathi@gmail.com>
2024-10-04 22:45:20 +0530

    Working cart increment and decrement

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -47,38 +47,44 @@ const Cart = () => {
                     </div>
                     <div className='flex flex-grow h-full w-full ml-3 bg-gradient-to-r from-white via-pink-100 to-pink-400 mx-auto mt-6 flex-col'>
 
-                        {
-                            add_cartitems2.map((product, index) => (
-                                <div className='flex flex-row border border-1 border-black m-1/2 h-[10rem] w-full'>
-                                    <img className='object-contain hover:scale-105 h-[7rem] w-[6rem] md:h-[9rem] md:w-[10rem] p-2' src={product.image_link} alt={product.product_name} />
-                                    <div className='flex flex-col ml-2 md:ml-5 w-full'>
-                                        <h1 className='flex text-black text-md md:text-2xl font-bold mt-2'>{product.product_name}</h1>
-                                        <div className='flex flex-col md:flex-row justify-between w-full  md:mx-2'>
-                                            {
+                        {(currentstate.isloggedin && currentstate.cartcount > 0) ? (
+                            <>{
+                                add_cartitems2.map((product, index) => (
+                                    <div className='flex flex-row border border-1 border-black m-1/2 h-[10rem] w-full'>
+                                        <img className='object-contain hover:scale-105 h-[7rem] w-[6rem] md:h-[9rem] md:w-[10rem] p-2' src={product.image_link} alt={product.product_name} />
+                                        <div className='flex flex-col ml-2 md:ml-5 w-full'>
+                                            <h1 className='flex text-black text-md md:text-2xl font-bold mt-2'>{product.product_name}</h1>
+                                            <div className='flex flex-col md:flex-row justify-between w-full  md:mx-2'>
+                                                {
 
-                                                (currentstate.usertxn.find(txn =>
-                                                    txn.user_id === currentstate.currentuser.id &&
-                                                    txn.product_id === product.product_id
-                                                )?.order_quantity > 0) && (
-                                                    <div key={index} className='flex flex-row justify-between ml-[2rem] gap-2 w-[3rem] h-[2rem]  items-center'>
-                                                        <button className='flex text-3xl -mt-[0.5rem]' onClick={() => cartUtils.decrementquantity(currentstate, product.product_id, dispatch)} >-</button>
-                                                        <input type="text" className='flex text-md h-[1.5rem] w-[2rem] border border-black text-center' value={currentstate.usertxn.find(txn => txn.user_id === currentstate.currentuser.id && txn.product_id === product.product_id)?.order_quantity || 0} />
-                                                        <button className='flex text-2xl -mt-[0.5rem]' onClick={() => cartUtils.incrementquantity(currentstate, product.product_id, dispatch)}>+</button>
-                                                    </div>
-                                                )
+                                                    (currentstate.usertxn.find(txn =>
+                                                        txn.user_id === currentstate.currentuser.id &&
+                                                        txn.product_id === product.product_id
+                                                    )?.order_quantity > 0) && (
+                                                        <div key={index} className='flex flex-row justify-between ml-[2rem] gap-2 w-[3rem] h-[2rem]  items-center'>
+                                                            <button className='flex text-3xl -mt-[0.5rem]' onClick={() => cartUtils.decrementquantity(currentstate, currentstate.usertxn, 'user_txn', product.product_id, dispatch)} >-</button>
+                                                            <input type="text" className='flex text-md h-[1.5rem] w-[2rem] border border-black text-center' value={currentstate.usertxn.find(txn => txn.user_id === currentstate.currentuser.id && txn.product_id === product.product_id)?.order_quantity || 0} />
+                                                            <button className='flex text-2xl -mt-[0.5rem]' onClick={() => cartUtils.incrementquantity(currentstate, currentstate.usertxn, 'user_txn', product.product_id, dispatch)}>+</button>
+                                                        </div>
+                                                    )
 
 
-                                            }
-                                            <h1 className='flex text-black text-md ml-[3.75rem] mt-4 md:mt-4 mr-[1rem] md:mr-[2rem] md:text-lg font-bold  mt-2 mr-10'>Price: {product.price}</h1>
+                                                }
+                                                <h1 className='flex text-black text-md ml-[3.75rem] mt-4 md:mt-4 mr-[1rem] md:mr-[2rem] md:text-lg font-bold  mt-2 mr-10'>Price: {product.price}</h1>
+                                            </div>
+
                                         </div>
+                                    </div>))
+                            }
 
-                                    </div>
-                                </div>))
-                        }
-                        <div className='flex border  border-t-1  border-black bg-gradient-to-r from-white via-blue-100 to-blue-400 h-[5rem] w-full'>
-                            <h2 className='flex text-lg md:text-2xl font-bold text-black items-center justify-center w-full mx-7'>
-                                <span className='flex w-full '>Total Cart Value</span> <span className='flex'>${currentstate.totalcartvalue}</span></h2>
-                        </div>
+                                < div className='flex border  border-t-1  border-black bg-gradient-to-r from-white via-blue-100 to-blue-400 h-[5rem] w-full'>
+                                    <h2 className='flex text-lg md:text-2xl font-bold text-black items-center justify-center w-full mx-7'>
+                                        <span className='flex w-full '>Total Cart Value</span> <span className='flex'>${currentstate.totalcartvalue}</span></h2>
+                                </div>
+                            </>
+                        ) : (
+                            <div className='flex'></div>
+                        )}
 
                     </div>
 
@@ -88,7 +94,7 @@ const Cart = () => {
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 
 }
