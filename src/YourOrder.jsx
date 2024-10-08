abffbdc3f701829073ad37075af461350242365e
indumathii <neelaindumathi@gmail.com>
2024-10-08 21:21:39 +0530

    working your orders responsive

--- src/YourOrder.jsx
+++ src/YourOrder.jsx
@@ -32,20 +32,20 @@ const YourOrder = () => {
 
     return (
         <div className={`${currentstate.isMenuVisible ? 'relative -z-10' : 'relative'} `}>
-            <div className='flex flex-grow relative min-h-screen min-w-screen flex-col justify-center items-center bg-[linear-gradient(45deg,_#4db6ac,_#80cbc4,_#4fc3f7,_#64b5f6,_#9575cd)] mt-[7rem] lg:mt-[3rem]'>
+            <div className='flex flex-grow relative min-h-screen w-[80vw] flex-col justify-center items-center bg-[linear-gradient(45deg,_#4db6ac,_#80cbc4,_#4fc3f7,_#64b5f6,_#9575cd)] mt-[7rem] lg:mt-[3rem]'>
 
                 <div className='flex  text-black flex-grow text-2xl h-auto w-full font-bold justify-center  items-center underline p-5'>ORDER HISTORY</div>
 
-                <div className='flex  pb-10 mb-10 md:-ml-[35rem] flex-grow w-1/2 md:-mt-[40rem] lg:-mt-[11rem] '>
-                    <div className='flex flex-grow h-full w-1/2  mt-[15rem] flex-col mx-auto justify-center items-center p-4 ml-6'>
+                <div className='flex  pb-10 mb-10 md:ml-[0rem] flex-grow w-1/2 md:w-full md:mt-[2rem] lg:-mt-[11rem] '>
+                    <div className='flex flex-grow h-full w-full  mt-[7rem] flex-col items-center p-4 md:ml-6 md:mt-[10rem]'>
 
                         {
                             currentstate.currentcart_txns.map(product => (
-                                <div className='flex flex-row bg-white m-1/2 h-[10rem] w-full mb-2'>
-                                    <img className='object-cover hover:scale-105 h-[7rem] w-[6rem] md:h-[9rem] md:w-[10rem] p-2' src={product.image_link} alt={product.product_name} />
-                                    <div className='flex  flex-col ml-2 md:ml-5 w-full'>
-                                        <h1 className='flex text-black text-md md:text-md font-bold mt-2'>{product.product_name}</h1>
-                                        <h1 className='flex text-black text-sm font-bold italic text-red-600 mt-[3rem] ml-[12rem]'>Arriving on {`${new Date(new Date().setDate(new Date().getDate() + product.days_of_delivery)).getDate()}${["th", "st", "nd", "rd"][(new Date().getDate() + product.days_of_delivery) % 10 > 3 ? 0 : (new Date().getDate() + product.days_of_delivery) % 10]} ${new Date(new Date().setDate(new Date().getDate() + product.days_of_delivery)).toLocaleString('default', { month: 'long' })}`}</h1>
+                                <div className='flex flex-col md:flex-row bg-white mb-[10rem] h-[20rem] w-[15rem] mb-[1rem] md:w-3/4 md:h-[15rem]'>
+                                    <img className='object-contain hover:scale-105 h-[15rem] w-[10rem] md:h-[15rem] md:w-[10rem] p-2' src={product.image_link} alt={product.product_name} />
+                                    <div className='flex  flex-col  w-full md:mt-[5rem] md:ml-[3rem]'>
+                                        <h1 className='flex text-black text-xs md:text-md font-bold mt-2 mb-2'>{product.product_name}</h1>
+                                        <h1 className='flex text-sm font-bold italic text-red-600 ml-[2rem] md:mt-[2rem]  mb-2'>Arriving on {`${new Date(Date.now() + product.days_of_delivery * 24 * 60 * 60 * 1000).getDate()}${(day => (day > 3 && day < 21) ? 'th' : ['th', 'st', 'nd', 'rd'][day % 10] || 'th')(new Date(Date.now() + product.days_of_delivery * 24 * 60 * 60 * 1000).getDate())} ${new Date(Date.now() + product.days_of_delivery * 24 * 60 * 60 * 1000).toLocaleString('default', { month: 'long' })}`}</h1>
                                     </div>
                                 </div>
                             ))
